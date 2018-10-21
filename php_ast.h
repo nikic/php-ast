791cf4162bb82ac4d0eb213c398157efa61f39f4
Nikita Popov <nikita.ppv@gmail.com>
2018-10-21 23:32:04 +0200

    Back to dev

--- php_ast.h
+++ php_ast.h
@@ -7,7 +7,7 @@
 extern zend_module_entry ast_module_entry;
 #define phpext_ast_ptr &ast_module_entry
 
-#define PHP_AST_VERSION "1.0.0"
+#define PHP_AST_VERSION "1.0.1dev"
 
 #ifdef PHP_WIN32
 #	define PHP_AST_API __declspec(dllexport)
